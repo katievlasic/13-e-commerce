52427e41d08f0c681871e69fd7cb726b5112b505
katievlasic <118415802+katievlasic@users.noreply.github.com>
2023-03-15 17:51:51 -0500

    build out category-routes put and delete

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -21,7 +21,7 @@ router.get('/:id', async (req, res) => {
       include: [{ model: Product }]
     });
 
-    if (!catSingleData) {
+    if (!catData) {
       res.status(404).json({ message: 'No category found with this id!' });
       return;
     }
@@ -38,12 +38,26 @@ router.post('/', async (req, res) => {
     const catData = await Category.create(req.body);
     res.status(200).json(catData);
   } catch (err) {
-    res.status(400).json(err);
+    res.status(500).json(err);
   }
 });
 
 // UPDATE a category by its id
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
+  try {
+    const catData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!catData) {
+      res.status(404).json({ message: 'No category with this id!' });
+      return;
+    }
+    res.status(200).json(catData); // 400 within try
+  } catch (err) {
+    res.status(500).json(err); // catch always 500 since server related
+  }
 });
 
 // DELETE a category by its id
@@ -54,6 +68,10 @@ router.delete('/:id', async (req, res) => {
         id: req.params.id
       }
     });
+    if (!catData) {
+      res.status(404).json({message: 'No category with this id'});
+      return;
+    }
     res.status(200).json(catData);
   } catch (err) {
     res.status(500).json(err);
